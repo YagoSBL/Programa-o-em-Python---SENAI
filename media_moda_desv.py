05b130a67dfb7a8b70a5964f9e08d5e04adaccb6
YagoSBL <102097791+YagoSBL@users.noreply.github.com>
2025-11-11 21:59:54 -0300

    Add files via upload

---
+++ media_moda_desv.py
@@ -0,0 +1,30 @@
+import modulo  
+
+# Cálculo de notas com moda média e mediana
+l = [1,2,3,4,5,6,9]
+
+# Calculo de salário com moda media e mediana
+empresa1 = [1000,6000,1200,8000,1400]
+
+empresa2 = [5000,4000,3000,2000,7000]
+
+empresa3 = [1200,1300,8000,3000,15000]
+
+empresa4 = [1400,1750,2000,4500,5900]
+
+modulo.estatistica(l)
+modulo.estatistica1(l)
+modulo.estatistica2(l)
+
+modulo.estatistica(empresa1)
+modulo.estatistica(empresa2)
+modulo.estatistica(empresa3)
+modulo.estatistica(empresa4)
+modulo.estatistica1(empresa1)
+modulo.estatistica1(empresa2)
+modulo.estatistica1(empresa3)
+modulo.estatistica1(empresa4)
+modulo.estatistica2(empresa1)
+modulo.estatistica2(empresa2)
+modulo.estatistica2(empresa3)
+modulo.estatistica2(empresa4)
