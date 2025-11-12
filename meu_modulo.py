531ce845b4d52eefb9f65d94d359c8727ec95664
YagoSBL <102097791+YagoSBL@users.noreply.github.com>
2025-11-11 21:16:59 -0300

    import test - mercado+meu_modulo

---
+++ meu_modulo.py
@@ -0,0 +1,32 @@
+def mercado (lista_prod,lista_valores):
+    carrinho = []
+    meus_valores = []
+    per = input('Deseja pedir? ')
+    while per == 'sim':
+        produto = int(input(f'''
+                  1 {lista_prod [1]} - {lista_valores[1]}
+                  2 {lista_prod [2]} - {lista_valores[2]}
+                  3 {lista_prod [3]} - {lista_valores[3]}
+                  4 {lista_prod [4]} - {lista_valores[4]}
+                  5 {lista_prod [5]} - {lista_valores[5]}
+                  6 {lista_prod [6]} - {lista_valores[6]}
+                  '''))
+        carrinho.append(lista_prod[produto])
+        meus_valores.append(lista_valores[produto])
+        print(carrinho)
+        soma = sum(meus_valores)
+        print('R$ ', soma)
+        
+        per = input('Deseja continuar? ')
+
+    else: 
+        print('Obrigado, volte sempre! ')
+        print('Se a compra foi efetuada, vá até pagamento ')
+
+def pagamentos(forma_pag):
+    print(forma_pag)
+    escolha = int(input('Escolha a forma de pagamento '))
+    print('Sua forma de pagamento é ', forma_pag[escolha])
+
+def despedir(nome):
+    return f'Obrigado. volte sempre {nome}'
