e268d5c749bc5f48ff67e105503599ca74d4f931
YagoSBL <102097791+YagoSBL@users.noreply.github.com>
2025-11-12 21:40:34 -0300

    Add files via upload

---
+++ aula14.py
@@ -0,0 +1,23 @@
+# with open('aula14.txt', 'r') as arq:
+#     c = arq.read()
+#     print(c)
+
+# with open('texto', 'w') as arq:
+#     print(arq)
+
+# with open('novo', 'w') as arq:
+#     os.mkdir('teste')
+
+# os.rename('text.txt', 'testem.txt')
+
+# with os.scandir('copy path', \\\\) as x:
+#   for n in x:
+#       print(n)
+
+
+# - Remoção de arquivos/diretórios
+#import shutil
+# shutil.rmtree('')
+
+# - cópia de pastas
+# shutil.copytree('')
