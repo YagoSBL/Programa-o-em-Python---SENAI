a33771dc17103ed52c06b4b0961a0fa31f552dc7
YagoSBL <102097791+YagoSBL@users.noreply.github.com>
2025-11-12 21:16:05 -0300

    Add files via upload

---
+++ aula13.py
@@ -0,0 +1,9 @@
+import modulo_aula13
+modulo_aula13.num_aleatorio()
+modulo_aula13.n_rand()
+modulo_aula13.nmr_rand()
+modulo_aula13.ctg_regr()
+modulo_aula13.nm_par()
+modulo_aula13.tabu()
+modulo_aula13.regr()
+
